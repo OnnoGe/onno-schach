39bc73f1f009abc44090fd16a6a807ff24f712a9
OnnoGe <onno.gellermann.xy@gmail.com>
2026-06-13 00:16:33 +0200

    Update main.php

--- api/main.php
+++ api/main.php
@@ -12,6 +12,7 @@ echo '<!DOCTYPE html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="assets/KW.png">
         <link rel="stylesheet" href="/chess.css">
     </head>
     <body>';
